a98454ae2c47072165eb580a8e5b15f7885287e0
Sarah Proctor <sproctor950@gmail.com>
2024-04-01 15:48:59 -0700

    :white_check_mark: adds request responses for 422 and corresponding specs

--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -21,6 +21,19 @@ RSpec.describe 'Cats', type: :request do
       expect(cat.valid?).to eq(true)
       expect(response).to have_http_status(200)
     end
+    it 'returns a http status error for an invalid cat' do
+      post cats_path, params: {
+        cat: {
+          name: nil,
+          age: nil,
+          enjoys: nil,
+          image: nil
+        }
+      }
+      cat = Cat.where(name: nil).first
+      expect(cat).to eq(nil)
+      expect(response).to have_http_status(422)
+    end
   end
   describe 'PATCH /update' do
     it 'return a http status of success makes valid updates to a cat' do
@@ -48,18 +61,41 @@ RSpec.describe 'Cats', type: :request do
       expect(cat.image).to eq('https://images.unsplash.com/photo-1577023311546-cdc07a8454d9?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
       expect(response).to have_http_status(200)
     end
+    it 'return a http status error when maling invalid updates to a cat' do
+      post cats_path, params: {
+        cat: {
+          name: 'Test cat for patch',
+          age: 4,
+          enjoys: 'testing attribute for enjoys',
+          image: 'https://images.unsplash.com/photo-1543852786-1cf6624b9987?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        }
+      }
+      cat = Cat.where(name: 'Test cat for patch').first
+      patch cat_path(cat), params: { 
+        cat: { 
+          name: nil, 
+          age: nil, 
+          enjoys: nil, 
+          image: nil
+        } 
+      }
+      cat = Cat.where(name: nil).first
+      expect(cat).to eq(nil)
+      expect(response).to have_http_status(422)
+    end
   end
   describe 'DELETE #destroy' do
     it 'deletes the cat' do
       cat = Cat.create(
-        name: 'Test Cat for delete request', 
+        name: 'Test cat for delete request', 
         age: 5, 
         enjoys: 'Test for enjoys attribute', 
         image: 'https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
       )
       delete cat_path(cat)
-      cat = Cat.where(name: 'Test Cat for delete request').first
+      cat = Cat.where(name: 'Test cat for delete request').first
       expect(cat).to eq(nil)
+      expect(response).to have_http_status(204)
     end
   end
 end
